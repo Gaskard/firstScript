775dcee7514f2f35a264e387a3dafba6f2d92d73
Gaskard <dimacr137@gmail.com>
2023-07-30 14:41:55 +0300

    add privat and genre

---
+++ js/scriptt.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const str = 'teSt';
+
+
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+console.log(logg.slice(6));
+
+console.log(logg.substring(6));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+/* console.log(parseInt(test)); */
+
+console.log(parseFloat(test));
